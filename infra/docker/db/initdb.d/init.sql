c5e9ac5d413a560e89d8cd3ef132ec83e0ac6c1e
Shinnosuke Kodani <kocchi.jan12@gmail.com>
2024-11-12 23:35:47 +0900

    [fix] 旧リポジトリ名に関連する記述を新リポジトリ名準拠に修正

--- infra/docker/db/initdb.d/init.sql
+++ infra/docker/db/initdb.d/init.sql
@@ -1,5 +1,5 @@
 -- .env, .env.example, .env.testing など、DB_DATABASEの値に合わせること
-CREATE DATABASE IF NOT EXISTS python_stations_4;
-CREATE DATABASE IF NOT EXISTS python_stations_4_test;
-GRANT ALL ON python_stations_4.* TO 'dev'@'%';
-GRANT ALL ON python_stations_4_test.* TO 'dev'@'%';
+CREATE DATABASE IF NOT EXISTS python_be_syokyu;
+CREATE DATABASE IF NOT EXISTS python_be_syokyu_test;
+GRANT ALL ON python_be_syokyu.* TO 'dev'@'%';
+GRANT ALL ON python_be_syokyu_test.* TO 'dev'@'%';
